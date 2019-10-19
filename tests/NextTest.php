f877b6285af7ae3dcbffee4d82e6395d63bb3661
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-10-19 11:38:38 +0200

    Add vVERSION output as convenience

--- tests/NextTest.php
+++ tests/NextTest.php
@@ -25,6 +25,20 @@ final class NextTest extends TestCase
             '1.1.0',
             '1.0.1',
         ];
+
+        yield 'v0.1.0' => [
+            'v0.1.0',
+            '1.0.0',
+            '0.2.0',
+            '0.1.1',
+        ];
+
+        yield 'v1.0.0' => [
+            'v1.0.0',
+            '2.0.0',
+            '1.1.0',
+            '1.0.1',
+        ];
     }
 
     /**
@@ -36,10 +50,13 @@ final class NextTest extends TestCase
     {
         $output = Next::run($version);
         $output = \str_replace(\PHP_EOL, '', $output);
-        [$_, $mayor, $minor, $patch] = \explode('::set-output name=', $output);
+        [$_, $mayor, $minor, $patch, $mayorV, $minorV, $patchV] = \explode('::set-output name=', $output);
 
         self::assertSame('mayor::' . $expectedMayor, $mayor, 'mayor');
         self::assertSame('minor::' . $expectedMinor, $minor, 'minor');
         self::assertSame('patch::' . $expectedPatch, $patch, 'patch');
+        self::assertSame('v_mayor::v' . $expectedMayor, $mayorV, 'v_mayor');
+        self::assertSame('v_minor::v' . $expectedMinor, $minorV, 'v_minor');
+        self::assertSame('v_patch::v' . $expectedPatch, $patchV, 'v_patch');
     }
 }
